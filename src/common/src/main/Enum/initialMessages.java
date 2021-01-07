c25723413ad37d067793e9e6e9f2da50b7a84392
Thor Dueholm <s194589@student.dtu.dk>
2021-01-07 17:09:06 +0100

    Added creating inbox in server

---
+++ src/common/src/main/Enum/initialMessages.java
@@ -0,0 +1,7 @@
+package common.src.main.Enum;
+
+// These are the messages a player can initially send
+public enum initialMessages {
+    STARTGAME,
+    NEWPLAYER
+}
--- src/common/src/main/Server.java
+++ src/common/src/main/Server.java
@@ -2,10 +2,15 @@ package common.src.main;
 
 import org.jspace.*;
 
+import java.util.HashMap;
+import common.src.main.Enum.initialMessages;
+
 public class Server {
 
     protected String currentWord; //The word which is being drawn
-
+    protected static int playerAmount = 0;
+    protected static HashMap<Integer,Space> playerInboxes;
+    protected SpaceRepository repo;
 
     public static void main(String[] args) {
 
@@ -23,15 +28,47 @@ public class Server {
             System.out.println("Opening repository gate at " + gateUri + "...");
             repository.addGate(gateUri);
 
+            //Waiting on game to start
+            Template initialMessageTemplate = new Template (new FormalField(String.class),
+                                                            new FormalField(initialMessages.class));  //Get name,enum
+
+            while (true) {
+                //Object[] message = chat.get(initialMessageTemplate) //TODO: why does this not work??
+                Object[] message = chat.get(new FormalField(String.class), new FormalField(initialMessages.class));
+                if (message[1].equals(initialMessages.NEWPLAYER)) {
+                    addplayer(message[0].toString(),repository); //add player and generate their inbox
+                } else if (message[1].equals(initialMessages.STARTGAME)) {
+                    break;
+                }
+            }
+
+            //Chat function
             while (true) {
                 Object[] t = chat.get(new FormalField(String.class), new FormalField(String.class)); //Get messages
                 System.out.println(t[0] + ": " + t[1]);
             }
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
 
+    }
+
+    private static void addplayer(String name, SpaceRepository repo) {
+        Space inbox = generateInbox(name, repo);
+        playerInboxes.put(playerAmount,inbox);
+        playerAmount++;
+    }
+
+    private static Space generateInbox(String name, SpaceRepository repo) {
+        Space inbox = new SequentialSpace();
+        repo.add(createName(name),inbox);
+        return inbox;
+    }
+
+    private static String createName(String name) {
+        return name + "-" + "playerAmount";
     }
 }
 
