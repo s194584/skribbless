83a5273e3d0feef208d9f7c855f29010b53d830d
MaltheOrberg <s194584@student.dtu.dk>
2021-01-11 09:46:23 +0100

    Created UserViewCell for a ListView Added ui ids to usercard.fxml

---
+++ src/common/src/main/Client/UserListViewCell.java
@@ -0,0 +1,60 @@
+package common.src.main.Client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public class UserListViewCell extends ListCell<User> {
+
+    @FXML
+    private Label characterLabel;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label pointLabel;
+
+    @FXML
+    private Pane root;
+
+
+    private FXMLLoader fxmlLoader;
+
+    @Override
+    protected void updateItem(User user, boolean b) {
+        super.updateItem(user, b);
+
+        setText(null);
+
+        // Return if there is no user or the item is empty
+        if(user==null|| b){
+            setGraphic(null);
+            return;
+        }
+
+        // Load usercard if it is the first time loading
+        if(fxmlLoader == null){
+            try {
+                fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("/usercard.fxml"));
+                fxmlLoader.setController(this);
+                fxmlLoader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Set the ui for the card
+        characterLabel.setText(user.getCharacter());
+        nameLabel.setText(user.getName());
+        pointLabel.setText(""+user.getScore());
+
+        // Set the cell graphic to root pane
+        setGraphic(root);
+    }
+}
